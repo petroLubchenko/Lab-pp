1750fa28217bb749bf1f5d2a1e174a52f6d11550
petroLubchenko <lubchenko.petro19@gmail.com>
2016-12-25 21:39:59 +0200

    Add files via upload

---
+++ Lab.js
@@ -0,0 +1,23 @@
+
+
+let musicians = ('Ludwig van Beethoven', 'Wolfgang Amadeus Mozart', 'Frank Sinatra', 'Johann Sebastian Bach', 'Richard Wagner',
+                 'Joseph Haydn', 'Franz Schubert', 'Robert Schumann', 'Piotr Illitch Tchaïkovsky', 'Georg Friedrich Händel',
+                 'Felix Mendelssohn-Bartholdy', 'Igor Stravinsky', 'Frédéric Chopin', 'Franz Liszt', 'Antonín Dvořák',
+                 'Giovanni Pierluigi da Palestrina', 'Richard Strauss', 'Dmitri Shostakovich', 'Sergej S. Prokofiew', 'Giuseppe Verdi',
+                 'Antonio Vivaldi', 'Claudio Monteverdi', 'Christoph Willibald Gluck', 'Camille Saint-Saëns', 'Georg Philipp Telemann');
+
+let musicians2 = musicians.filter(i => (i >= 0041 && i <= 'Z') || (i >= 0061 && i <= 'z'))
+
+
+
+
+
+
+				 
+                 
+function filter (item) {
+	return (item >= 0041 && item <= 'Z' || item >= 0061 && item <= 'z');
+	
+	
+	
+}
